091b63770881eb7fd3740a58ba1994d543bab04e
Tyler Norman <TylerNorman881@hotmail.com>
2024-11-18 19:45:16 -0500

    Initial Commit
    Tinkering around with accessing the Old School Runescape Wiki's API for Grand Exchange item buy/sell values, IDs, and icon PNGs.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import AlchPreview from './AlchPreview.js';
+
+//OSRS Grand Exchange API endpoint: prices.runescape.wiki/api/v1/osrs
+//Abyssal Whip ID for testing: https://prices.runescape.wiki/api/v1/osrs/latest?id=4151
 
 function App() {
   return (
@@ -8,6 +12,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          <AlchPreview/>
+          
         </p>
         <a
           className="App-link"
