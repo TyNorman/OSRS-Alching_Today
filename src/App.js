ca3afcbd5c61db8cd258fa7f4e56ed66d1f8d547
Tyler Norman <TylerNorman881@hotmail.com>
2024-11-19 20:33:17 -0500

    AlchPreview Reorganizing, Item Sorting by High Alch Values
    Moved AlchPreview into its own folder. Implemented array population of all required item info to determine the optimal high alch options. Currently sorting options by the gap in high alch value to the current average Grand Exchange value.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import AlchPreview from './AlchPreview.js';
+import AlchPreview from './AlchPreview/AlchPreview.js';
 
 //OSRS Grand Exchange API endpoint: prices.runescape.wiki/api/v1/osrs
 //Abyssal Whip ID for testing: https://prices.runescape.wiki/api/v1/osrs/latest?id=4151
@@ -9,21 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-
           <AlchPreview/>
-          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
